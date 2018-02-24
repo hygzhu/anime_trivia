1e259bafaef60c3b39a375b832c4a8855800e675
Haoyang <hygzhu@gmail.com>
2018-02-23 21:48:39 -0500

    Renames and new component file (Broken)

---
+++ src/components/app.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import Trivia from './trivia';
+import Menu from "./menu"
+import Score from "./score"
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            screen: "menu",
+            mode: "visible",
+            choices: 4,
+            score: 0
+        }
+    }
+
+    screen() {
+        const screen = this.state.screen;
+        const modeMultiplier = this.state.mode == "visible" ? 0.1 : 1;
+        const choicesMultiplier = this.state.choices / 4;
+        const multiplier = 1 * modeMultiplier * choicesMultiplier;
+
+        if (screen == "trivia") {
+            return <Trivia
+                mode={this.state.mode}
+                choices={this.state.choices}
+                multiplier={multiplier}
+                scoreCallback={this.scoreCallback}/>
+        } else if (screen == "menu") {
+            return (
+                <div>
+                  <Menu 
+                    multiplier={this.state.multiplier}
+                    modeMultiplier={this.state.modeMultiplier}
+                    choicesMultiplier={this.state.choicesMultiplier}
+                    choices={this.state.choices}
+                    mode={this.state.mode}
+                    changeState={this.changeState}
+                    setMode={this.setMode}
+                    setChoices={this.setChoices}
+                  />
+                </div>
+            );
+        } else if (screen == "score") {
+            return (
+                <div>
+                    <h1>congratulations</h1>
+                    <h2>Your score was: {this.state.score}</h2>
+                    <button
+                        onClick={this.changeState.bind(this, "menu")}>Back to menu</button>
+                </div>
+            );
+        }
+    }
+
+    changeState = (newState) => {
+        this.setState({screen: newState});
+    }
+
+    setMode = (newMode) => {
+        this.setState({mode: newMode});
+    }
+
+    setChoices = (event) => {
+        this.setState({choices: newMode});
+    }
+
+    scoreCallback = (data) => {
+        this.setState({score: data, screen: "score"});
+    }
+
+    render() {
+        return (
+            <div>
+                {this.screen()}
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/components/menu.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+export default class Menu extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <div>
+                    <h1>Anime Song Trivia</h1>
+                    <button onClick={this.changeState("trivia")}>Play</button>
+                    <h2>Points Multiplier: {this.props.multiplier}x</h2>
+                    <br />
+                    <br />
+                    <div>
+                        <h3>Mode: {this.props.mode}({this.props.modeMultiplier}x)</h3>
+                        <select onChange={this.changeMode(value)}>
+                            <option value="visible">visible</option>
+                            <option value="hidden">hidden</option>
+                        </select>
+                        <h3>Choices: {this.props.choices}({this.props.choicesMultiplier / 4}x)</h3>
+                        <select onChange={this.setChoices(value)}>
+                            <option value={4}>Few</option>
+                            <option value={8}>Many</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    changeState(newState) {
+        this.props.changeState(newState);
+    }
+
+    changeMode(newMode) {
+        this.props.setMode(newMode);
+    }
+
+    setChoices(newChoices) {
+        this.props.setChoices(newChoices);
+    }
+}
+
