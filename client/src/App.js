576a51aeae41ef8be0986ef1922b0b1596b3d4ce
Haoyang <hygzhu@gmail.com>
2018-03-15 20:00:38 -0400

    Added socket.io

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import socketIOClient from 'socket.io-client'
 import './App.css';
 
 import Game from './components/game/game.component';
@@ -7,6 +8,9 @@ import Game from './components/game/game.component';
 
 export default class App extends Component {
   render () {
+
+    const socket = socketIOClient("http://localhost:4001")
+
     return (
       <div style={{ textAlign: "center"}}>
         <Game/>
